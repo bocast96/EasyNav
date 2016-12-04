0fe630735cdc5cfbd4801311e0803f8f3ad9d1f5
Rishabh Sharan <rsharan@terpmail.umd.edu>
2016-12-04 16:48:42 -0500

    added search functionality

--- app/src/main/java/com/example/eznav/easynav/MapsActivity.java
+++ app/src/main/java/com/example/eznav/easynav/MapsActivity.java
@@ -4,6 +4,8 @@ import android.*;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
@@ -11,6 +13,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.SearchView;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -22,59 +25,66 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener , LocationListener{
+import java.util.List;
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static final long LOCATION_REFRESH_TIME = 100;
+    private static final float LOCATION_REFRESH_DISTANCE = .01f;
     private GoogleMap mMap;
-    GoogleApiClient mGoogleApiClient = null;
-    Location mCurrentLocation;
-    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    // The desired interval for location updates. Inexact. Updates may be more or less frequent.
-    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
-    // The fastest rate for active location updates. Exact. Updates will never be more frequent
-    // than this value.
-    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
-            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
-    boolean mLocationPermissionGranted = false;
-    private LocationRequest mLocationRequest;
+    private double home_long;
+    private double home_lat;
+    private LatLng latLng;
+    private String addressText;
+    private MarkerOptions markerOptions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+
+        final SearchView searchView = (SearchView) findViewById(R.id.search_button);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                                              @Override
+                                              public boolean onQueryTextSubmit(String query) {
+                                                  String g = query;
+
+                                                  Geocoder geocoder = new Geocoder(getBaseContext());
+                                                  List<Address> addresses = null;
+
+                                                  try {
+                                                      // Getting a maximum of 3 Address that matches the input
+                                                      // text
+                                                      addresses = geocoder.getFromLocationName(g, 3);
+                                                      if (addresses != null && !addresses.equals("")) {
+                                                          search(addresses);
+                                                      }
+                                                      searchView.clearFocus();
+
+                                                  } catch (Exception e) {
+
+                                                  }
+                                                  return true;
+                                              }
+
+                                              @Override
+                                              public boolean onQueryTextChange(String newText) {
+                                                  return false;
+                                              }
+                                          });
+
+                // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-        onStart();
-        mLocationRequest = new LocationRequest();
-
-        /*
-         * Sets the desired interval for active location updates. This interval is
-         * inexact. You may not receive updates at all if no location sources are available, or
-         * you may receive them slower than requested. You may also receive updates faster than
-         * requested if other applications are requesting location at a faster interval.
-         */
-        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
-
-        /*
-         * Sets the fastest rate for active location updates. This interval is exact, and your
-         * application will never receive updates faster than this value.
-         */
-        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
-
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
     }
 
@@ -92,17 +102,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         Log.i("TEST", "TEST");
         mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
 
         // Add a marker in Sydney and move the camera
         //LatLng sydney = new LatLng(-34, 151);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        LatLng collegePark = new LatLng(38.990669,-76.936418);
-        mMap.addMarker(new MarkerOptions().position(collegePark).title("Marker in CP"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(collegePark));
-        //mMap.zoo
 
+
+        LatLng collegePark = new LatLng(38.9907439,-76.9362396);
+        LatLng avwilliams_bike_rack_front = new LatLng(38.9906623,-76.9364820);
+        LatLng avwilliams_bike_rack_side = new LatLng(38.9901492,-76.9365354);
+        mMap.addMarker(new MarkerOptions().position(collegePark).title("Marker in CP"));
+
+        mMap.addMarker(new MarkerOptions().position(avwilliams_bike_rack_front).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.addMarker(new MarkerOptions().position(avwilliams_bike_rack_side).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(collegePark));
+        float zoomLevel = 17.0f; //This goes up to 21
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(collegePark, zoomLevel));
     }
 
 
@@ -120,18 +150,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
+    protected void search(List<Address> addresses) {
+
+        Address address = addresses.get(0);
+        home_long = address.getLongitude();
+        home_lat = address.getLatitude();
+        latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+        addressText = String.format(
+                "%s, %s",
+                address.getMaxAddressLineIndex() > 0 ? address
+                        .getAddressLine(0) : "", address.getCountryName());
+
+        markerOptions = new MarkerOptions();
+
+        markerOptions.position(latLng);
+        markerOptions.title(addressText);
+
+        //mMap.clear();
+        mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+        //locationTv.setText("Latitude:" + address.getLatitude() + ", Longitude:"
+        //       + address.getLongitude());
+
+
     }
 
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        mCurrentLocation = location;
-    }
 }
